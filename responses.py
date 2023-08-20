37119e79dae5d126af838652cd145835c543a8f6
hopster <hopster@student.ubc.ca>
2023-08-19 22:01:05 -0700

    Made template of bot

---
+++ responses.py
@@ -0,0 +1,10 @@
+import random
+
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+
+    if p_message == 'roll':
+        return str(random.randint(1,6))
+
+    return 'I don\'t understand'
